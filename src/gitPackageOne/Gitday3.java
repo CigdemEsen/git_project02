74fdf6f1c7bf44cf6004f2820b456cf65da45fb8
Cigdem Esen <ekremnilufer2@gmail.com>
2020-03-15 16:25:01 -0400

    second push from feature branch

--- src/gitPackageOne/Gitday3.java
+++ src/gitPackageOne/Gitday3.java
@@ -6,6 +6,7 @@ public class Gitday3 {
 		
 		System.out.println("my first changes on local master");
 		System.out.println("this is my feature branch");
+		System.out.println("my second push from local feature branch");
 	}
 
 }
